46ddbaf6a1d718720ca298953b19040c5fc24c99
nicola.vurchio <nicola.vurchio@iad2.it>
2022-05-25 10:49:49 +0200

    updated react-ghost-auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,27 @@ import AuthenticationProvider, {
 } from '@iad-os/react-ghost-auth';
 import axios from 'axios';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import authConfig from './authConfig';
 import ModalLogin from './components/ModalLogin';
 import AppThemeProvider from './contexts/AppTheme';
 import Routes from './routes/Routes';
 
 const App: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <AppThemeProvider>
-      <AuthenticationProvider config={authConfig} axios={axios}>
+      <AuthenticationProvider
+        config={authConfig}
+        axios={axios}
+        onRoute={route => {
+          const local = `${window.location.protocol}//${
+            window.location.hostname
+          }${window.location.port ? `:${window.location.port}` : ''}`;
+          navigate(route.replace(local, ''), { replace: true });
+        }}
+      >
         <Routes />
         <LogginIn>
           <h2>🔄 Loading...</h2>
