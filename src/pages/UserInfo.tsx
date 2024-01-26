705096743c933937a93a3a76266407d5a9abaee6
Nicola Vurchio <nicola.vurchio@iad2.it>
2024-01-26 15:20:34 +0100

    minor fix

--- src/pages/UserInfo.tsx
+++ src/pages/UserInfo.tsx
@@ -1,14 +1,24 @@
 import { useUserInfo } from '@iad-os/react-ghost-auth';
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography, useTheme } from '@mui/material';
+import Editor from '@monaco-editor/react';
 
 const UserInfo = () => {
   const userInfo = useUserInfo<any>();
+
+  const { palette } = useTheme();
 
   return (
     <Stack>
       <Typography variant="h4">User info </Typography>
       <Box>
-        <pre>{JSON.stringify(userInfo, null, 4)}</pre>
+        <Editor
+          height={'80vh'}
+          width={'100%'}
+          language="json"
+          value={JSON.stringify(userInfo, null, 4)}
+          theme={palette.mode === 'dark' ? 'vs-dark' : 'light'}
+          options={{ domReadOnly: true, readOnly: true }}
+        />
       </Box>
     </Stack>
   );
